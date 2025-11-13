358428c99510787abfd842b2c5fd7a060fa2b3f5
saddam848811 <sh848811@gmail.com>
2025-11-13 10:20:52 +0530

    commit

---
+++ ck_assesments/java_assesment_3/JavaEight.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+
+
+
+
+@FunctionalInterface
+interface  NumberRule{
+
+    boolean apply(int n);
+
+}
+
+class JavaEight {
+
+    public static void main(String[] args) {
+
+        List<Integer> nums = Arrays.asList(3, 10, 15, 8, 21, 14, 7);
+        System.out.println("saddam");
+    }
+}
